bd938b5d77c3d84907286580a4976e007fe1a4f5
Enes Atar <enes9gs@hotmail.com>
2026-01-18 21:53:08 +0300

    update

--- android/app/src/main/kotlin/com/example/flutter_application_6/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_6/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.flutter_application_6
 
 import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity(){}
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import java.util.TimeZone
 
+
 class MainActivity: FlutterFragmentActivity() {
+
     private val CHANNEL = "flutter.native/helper"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
